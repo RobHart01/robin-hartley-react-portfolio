7cdde089e7cd2ccaaaab6cd85210d00e4608a0c5
Robin Hartley <robert.taku.hartley@gmail.com>
2019-08-12 14:40:45 -0700

    updated hobbies & technologies

--- src/Components/Bio/Hobbies/Hobbies.js
+++ src/Components/Bio/Hobbies/Hobbies.js
@@ -5,7 +5,14 @@ import './Hobbies.css';
 function Hobbies() {
     return (
         <div className="Hobbies">
-            <p>This is the Hobby Component.</p>
+            <p className="">Things I enjoy doing:</p>
+            <ul className="">
+                <li className="">Ordering an espresso from the local downtown Seattle coffee shops.</li>
+                <li className="">Doing anything related to photography or videography.</li>
+                <li className="">I spend a lot of time thinking about architecture. Not code, well that too, but more like building wise.</li>
+                <li className="">I'm from Japan, so making trips there is always something I look foward to.</li>
+                <li className="">My spotify playlist is my treasure.</li>
+            </ul>
         </div>
     )
 }
--- src/Components/Bio/Technologies/Technologies.js
+++ src/Components/Bio/Technologies/Technologies.js
@@ -5,12 +5,46 @@ import './Technologies.css';
 function Technologies() {
     return (
         <div className="Technologies">
-            <p>Most of my experience is in JavaScript, but other than that, here are all of the technologies in which I am familiar and comfortable with.</p>
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+            <p className="">Most of my experience is in JavaScript, but other than that, here are all of the technologies in which I am familiar and comfortable with.</p>
+            <ul className="">
+                <li>Languages:
+                <li className="">C#</li>
+                    <li className="">CSS3</li>
+                    <li className="">HTML5</li>
+                    <li className="">JavaScript</li>
+                    <li className="">Python</li>
+                    <li className="">TypeScript</li>
+                </li>
+                <li>Frameworks/Libraries:
+                    <li className="">Angular</li>
+                    <li className="">BootStrap</li>
+                    <li className="">Bulma</li>
+                    <li className="">Django</li>
+                    <li className="">Express.js</li>
+                    <li className="">Flask</li>
+                    <li className="">jQuery</li>
+                    <li className="">Mongoose</li>
+                    <li className="">Next.js</li>
+                    <li className="">Node.js</li>
+                    <li className="">React</li>
+                    <li className="">Redux</li>
+                    <li className="">SASS/SCSS/LESS</li>
+                    <li className="">Socket.Io</li>
+                    <li className="">Vue.js</li>
+                    <li className="">Vuex</li>
+                </li>
+                <li>Databases:
+                    <li className="">MongoDB</li>
+                    <li className="">MySQL</li>
+                </li>
+                <li>Methodologies:
+                <li className="">Agile/Lean Software Development</li>
+                    <li className="">DOM manipulation</li>
+                    <li className="">MVC Framework</li>
+                    <li className="">OOP</li>
+                    <li className="">SCRUM</li>
+                    <li className="">TDD</li>
+                </li>
             </ul>
         </div>
     )
