af07332e77fee7e599dfc5f2a8c2be3d6cb6c95c
Robin Hartley <robert.taku.hartley@gmail.com>
2019-08-12 12:32:26 -0700

    finished up contact for now

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -6,10 +6,11 @@ import LinkedIn from '../../Assets/Images/linkedin-icon.png';
 function Contact() {
     return (
         <div className="Contact">
-                <p className="contact-title">Contact</p>
+            <p className="contact-title">Contact</p>
             <div className="contact-content-wrapper">
                 <p className="contact-content-1">If you would like to work with me or to just ask questions, I'm always open to them!</p>
                 <div className="contact-social-links">
+                    <p className="contact-social-content">Add/Follow me on!</p>
                     <a href="https://www.linkedin.com/in/robin-hartley/"><img className="contact-social-linkedin" src={LinkedIn} alt="linkedin icon" /></a>
                     <a href="https://github.com/RobHart01"><img className="contact-social-github" src={Github} alt="github icon" /></a>
                 </div>
